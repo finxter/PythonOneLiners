24a16b95c8b93204b277d8586724255ae84439b0
finxter <57671493+finxter@users.noreply.github.com>
2020-09-05 19:47:05 +0200

    Update one_liner_7.py

--- book/python_tricks/one_liner_7.py
+++ book/python_tricks/one_liner_7.py
@@ -1,1 +1,17 @@
+# Using Slice Assignment to Correct Corrupted Lists
 
+
+## Data
+visitors = ['Firefox', 'corrupted', 'Chrome', 'corrupted',
+            'Safari', 'corrupted', 'Safari', 'corrupted',
+            'Chrome', 'corrupted', 'Firefox', 'corrupted']
+
+## One-Liner
+visitors[1::2] = visitors[::2]
+
+## Result
+print(visitors)
+'''
+['Firefox', 'Firefox', 'Chrome', 'Chrome', 'Safari', 'Safari',
+'Safari', 'Safari', 'Chrome', 'Chrome', 'Firefox', 'Firefox']
+'''
