3bd6a3840c2ea30281a8926cb62f2b412f1ccf33
finxter <57671493+finxter@users.noreply.github.com>
2020-09-05 19:48:26 +0200

    Update one_liner_8.py

--- book/python_tricks/one_liner_8.py
+++ book/python_tricks/one_liner_8.py
@@ -1,1 +1,14 @@
+# Analyzing Cardiac Health Data with List Concatenation
 
+## Dependencies
+import matplotlib.pyplot as plt
+
+## Data
+cardiac_cycle = [62, 60, 62, 64, 68, 77, 80, 76, 71, 66, 61, 60, 62]
+
+## One-Liner
+expected_cycles = cardiac_cycle[1:-2] * 10
+
+## Result
+plt.plot(expected_cycles)
+plt.show()
