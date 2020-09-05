a99c06987436dde137ac55d7dd56a09c225671d8
finxter <57671493+finxter@users.noreply.github.com>
2020-09-05 19:51:46 +0200

    Create one_liner_9.py

---
+++ book/python_tricks/one_liner_9.py
@@ -0,0 +1,19 @@
+# Formatting Databases with the zip() Function
+
+
+## Data
+column_names = ['name', 'salary', 'job']
+db_rows = [('Alice', 180000, 'data scientist'),
+           ('Bob', 99000, 'mid-level manager'),
+           ('Frank', 87000, 'CEO')]
+
+## One-Liner
+db = [dict(zip(column_names, row)) for row in db_rows]
+
+## Result
+print(db)
+'''
+[{'name': 'Alice', 'salary': 180000, 'job': 'data scientist'},
+ {'name': 'Bob', 'salary': 99000, 'job': 'mid-level manager'},
+ {'name': 'Frank', 'salary': 87000, 'job': 'CEO'}]
+'''
