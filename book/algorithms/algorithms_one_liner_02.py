cfef8efd9e5d6e4fd2677de87bdd14c3ef12c6da
finxter <57671493+finxter@users.noreply.github.com>
2020-09-06 20:23:09 +0200

    Create algorithms_one_liner_02.py

---
+++ book/algorithms/algorithms_one_liner_02.py
@@ -0,0 +1,15 @@
+# Finding Palindromes with Lambda Functions and Negative Slicing
+
+
+## One-Liner
+is_palindrome = lambda phrase: phrase == phrase[::-1]
+
+## Result
+print(is_palindrome("anna"))
+print(is_palindrome("kdljfasjf"))
+print(is_palindrome("rats live on no evil star"))
+'''
+True
+False
+True
+'''
