0f5039ff6fd9f4f5ade8570544d563b32deebf50
finxter <57671493+finxter@users.noreply.github.com>
2020-09-06 19:52:04 +0200

    Create numpy_one_liner_09.py

---
+++ book/data_science/numpy_one_liner_09.py
@@ -0,0 +1,26 @@
+# Simple Association Analysis: People Who Bought X Also Bought Y
+
+
+## Dependencies
+import numpy as np
+
+## Data: row is customer shopping basket
+## row = [course 1, course 2, ebook 1, ebook 2]
+## value 1 indicates that an item was bought.
+basket = np.array([[0, 1, 1, 0],
+                   [0, 0, 0, 1],
+                   [1, 1, 0, 0],
+                   [0, 1, 1, 1],
+                   [1, 1, 1, 0],
+                   [0, 1, 1, 0],
+                   [1, 1, 0, 1],
+                   [1, 1, 1, 1]])
+
+## One-liner
+copurchases = np.sum(np.all(basket[:,2:], axis = 1)) / basket.shape[0]
+
+## Result
+print(copurchases)
+'''
+0.25
+'''
